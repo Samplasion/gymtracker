e125ab9fd45f5738a712693d6a022b5b3d72576b
Francesco <samplasion@gmail.com>
2024-01-15 20:08:04 +0100

    fix: I am literally so dumb and tired

--- lib/view/skeleton.dart
+++ lib/view/skeleton.dart
@@ -112,15 +112,15 @@ class _SkeletonViewState extends State<SkeletonView>
                   icon: const Icon(Icons.history_rounded),
                   label: "history.title".t,
                 ),
-                if (kDebugMode)
-                  NavigationDestination(
-                    icon: const Icon(Icons.settings_rounded),
-                    label: "settings.title".t,
-                  ),
-                const NavigationDestination(
-                  icon: Icon(Icons.bug_report),
-                  label: "Debug",
+                NavigationDestination(
+                  icon: const Icon(Icons.settings_rounded),
+                  label: "settings.title".t,
                 ),
+                if (kDebugMode)
+                  const NavigationDestination(
+                    icon: Icon(Icons.bug_report),
+                    label: "Debug",
+                  ),
               ],
               selectedIndex: _selectedIndex,
               onDestinationSelected: (i) => setState(() => _selectedIndex = i),

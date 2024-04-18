b34b7c2585c4c8dc264a8a3c4b9ffac2a0939c91
Francesco <samplasion@gmail.com>
2024-04-18 23:30:28 +0200

    build: Add auto intl downloader

---
+++ tools/download_intl.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# This script downloads the latest translations from the Accent server.
+# It is intended to be run from the root of the project.
+
+accent export
+
+# Commit the changes
+git add assets/i18n
+
+# Check if there are any changes to commit
+if git diff-index --quiet HEAD --; then
+  echo "No changes to commit"
+else
+  git commit -m "chore: update translations"
+  git push
+fi

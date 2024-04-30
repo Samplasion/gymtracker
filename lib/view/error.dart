232e23dcbb18b5597faf597a9c9f809b51d315d1
Francesco <samplasion@gmail.com>
2024-04-30 18:58:15 +0200

    fix: Back button on Android in ErrorView
    ...whereby you could simply... ignore the error

--- lib/view/error.dart
+++ lib/view/error.dart
@@ -14,91 +14,94 @@ class ErrorView extends GetWidget<ErrorController> {
   Widget build(BuildContext context) {
     ErrorViewArguments args =
         ModalRoute.of(context)!.settings.arguments as ErrorViewArguments;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("errorView.shortTitle".t),
-        automaticallyImplyLeading: false,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "errorView.title".t,
-              style: Get.textTheme.displayLarge,
-            ),
-            const SizedBox(height: 8),
-            Text(
-              "errorView.subtitle".t,
-              style: Get.textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 16),
-            Text("errorView.body".t),
-            if (kDebugMode) ...[
-              const SizedBox(height: 16),
-              FilledButton(
-                onPressed: () {
-                  Get.back();
-                },
-                child: const Text("Back"),
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("errorView.shortTitle".t),
+          automaticallyImplyLeading: false,
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "errorView.title".t,
+                style: Get.textTheme.displayLarge,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                "errorView.subtitle".t,
+                style: Get.textTheme.headlineMedium,
               ),
               const SizedBox(height: 16),
-              FilledButton(
-                onPressed: () {
-                  controller.dumpError(args);
-                },
-                child: const Text("Dump error to console"),
-              ),
-            ],
-            const SizedBox(height: 16),
-            Text(
-              "errorView.stack".t,
-              style: Get.textTheme.bodyMedium,
-            ),
-            const SizedBox(height: 16),
-            if (args.details != null) ...[
-              Text(
-                args.details!.exceptionAsString(),
-                style: TextStyle(
-                  color: context.colorScheme.onSurfaceVariant,
-                  fontFamily: "monospace",
-                  fontFamilyFallback: const ["Menlo", "Courier"],
+              Text("errorView.body".t),
+              if (kDebugMode) ...[
+                const SizedBox(height: 16),
+                FilledButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  child: const Text("Back"),
                 ),
-              ),
+                const SizedBox(height: 16),
+                FilledButton(
+                  onPressed: () {
+                    controller.dumpError(args);
+                  },
+                  child: const Text("Dump error to console"),
+                ),
+              ],
               const SizedBox(height: 16),
               Text(
-                args.details!.stack.toString(),
-                style: TextStyle(
-                  color: context.colorScheme.onSurfaceVariant,
-                  fontFamily: "monospace",
-                  fontFamilyFallback: const ["Menlo", "Courier"],
-                ),
-              ),
-            ] else
-              Text(
-                args.error.toString(),
-                style: TextStyle(
-                  color: context.colorScheme.onSurfaceVariant,
-                  fontFamily: "monospace",
-                  fontFamilyFallback: const ["Menlo", "Courier"],
-                ),
-              ),
-            if (args.stack != null)
-              Text(
-                "\n${args.stack!.toString()}",
-                style: TextStyle(
-                  color: context.colorScheme.onSurfaceVariant,
-                  fontFamily: "monospace",
-                  fontFamilyFallback: const ["Menlo", "Courier"],
-                ),
-              )
-            else
-              Text(
-                "errorView.noStack".t,
+                "errorView.stack".t,
                 style: Get.textTheme.bodyMedium,
               ),
-          ],
+              const SizedBox(height: 16),
+              if (args.details != null) ...[
+                Text(
+                  args.details!.exceptionAsString(),
+                  style: TextStyle(
+                    color: context.colorScheme.onSurfaceVariant,
+                    fontFamily: "monospace",
+                    fontFamilyFallback: const ["Menlo", "Courier"],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  args.details!.stack.toString(),
+                  style: TextStyle(
+                    color: context.colorScheme.onSurfaceVariant,
+                    fontFamily: "monospace",
+                    fontFamilyFallback: const ["Menlo", "Courier"],
+                  ),
+                ),
+              ] else
+                Text(
+                  args.error.toString(),
+                  style: TextStyle(
+                    color: context.colorScheme.onSurfaceVariant,
+                    fontFamily: "monospace",
+                    fontFamilyFallback: const ["Menlo", "Courier"],
+                  ),
+                ),
+              if (args.stack != null)
+                Text(
+                  "\n${args.stack!.toString()}",
+                  style: TextStyle(
+                    color: context.colorScheme.onSurfaceVariant,
+                    fontFamily: "monospace",
+                    fontFamilyFallback: const ["Menlo", "Courier"],
+                  ),
+                )
+              else
+                Text(
+                  "errorView.noStack".t,
+                  style: Get.textTheme.bodyMedium,
+                ),
+            ],
+          ),
         ),
       ),
     );

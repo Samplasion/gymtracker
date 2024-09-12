b1fdb1f7c66b80356a064d00b9ff0c038323de62
Francesco <samplasion@gmail.com>
2024-09-12 14:02:44 +0200

    fix: Avoid error when clearing calculator textbox

--- lib/view/utils/weight_calculator.dart
+++ lib/view/utils/weight_calculator.dart
@@ -96,8 +96,10 @@ class _WeightCalculatorState extends State<WeightCalculator>
                       ),
                       onChanged: (value) {
                         setState(() {
-                          _weightController.text = stringifyDouble(
-                              double.parse(value).clamp(0, 10000));
+                          var dbl = value.tryParseDouble();
+                          _weightController.text = dbl == null
+                              ? ""
+                              : stringifyDouble(dbl.clamp(0, 10000));
                           if (value.endsWith(".")) {
                             _weightController.text += ".";
                           }

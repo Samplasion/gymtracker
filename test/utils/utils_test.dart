8585f099a3367b06ad448a9b1304ae90e194d308
Francesco <samplasion@gmail.com>
2024-10-30 11:03:52 +0100

    test: Fix function name

--- test/utils/utils_test.dart
+++ test/utils/utils_test.dart
@@ -5,18 +5,18 @@ void main() {
   group("getLastDayOfWeek -", () {
     final DateTime today = DateTime(2024, 1, 1);
     test("should return today if today's day of week is the one provided", () {
-      expect(getLastDayOfWeek(today, DateTime.monday), DateTime(2024, 1, 1));
+      expect(getLastFirstDayOfWeek(today, DateTime.monday), DateTime(2024, 1, 1));
     });
     test("should return a day in the last week otherwise", () {
-      expect(getLastDayOfWeek(today, DateTime.tuesday), DateTime(2023, 12, 26));
+      expect(getLastFirstDayOfWeek(today, DateTime.tuesday), DateTime(2023, 12, 26));
       expect(
-          getLastDayOfWeek(today, DateTime.wednesday), DateTime(2023, 12, 27));
+          getLastFirstDayOfWeek(today, DateTime.wednesday), DateTime(2023, 12, 27));
       expect(
-          getLastDayOfWeek(today, DateTime.thursday), DateTime(2023, 12, 28));
-      expect(getLastDayOfWeek(today, DateTime.friday), DateTime(2023, 12, 29));
+          getLastFirstDayOfWeek(today, DateTime.thursday), DateTime(2023, 12, 28));
+      expect(getLastFirstDayOfWeek(today, DateTime.friday), DateTime(2023, 12, 29));
       expect(
-          getLastDayOfWeek(today, DateTime.saturday), DateTime(2023, 12, 30));
-      expect(getLastDayOfWeek(today, DateTime.sunday), DateTime(2023, 12, 31));
+          getLastFirstDayOfWeek(today, DateTime.saturday), DateTime(2023, 12, 30));
+      expect(getLastFirstDayOfWeek(today, DateTime.sunday), DateTime(2023, 12, 31));
     });
   });
 }
